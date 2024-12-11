3b098ba6df6dea1dfb68a76e3cfeae59469c6810
Rahul Gupta <rahul.gupta19970@gmail.com>
2024-12-12 02:18:41 +0530

    Assignment-03 done

--- Assignment/assignment-episode-03.js
+++ Assignment/assignment-episode-03.js
@@ -172,4 +172,11 @@ const HeaderComponentNew = ()=>(
        // episode3Root.render(<HeadingComponent3/>);
         episode3Root.render(<HeaderComponentNew/>);
         
-    
+       /* References
+        ● Babel: https://babeljs.io/
+        ● Attribute Type:
+        https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script#attr-type
+        ● JS Modules:
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+        ● Babel Playground: https://babeljs.io/repl#
+        ● React without JSX: https://reactjs.org/docs/react-without-jsx.html*/
