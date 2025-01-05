1e90b332fb5f3bdfae54a0efd6b4e90be375db1c
Rahul Gupta <rahul.gupta19970@gmail.com>
2025-01-06 01:17:36 +0530

    Episode-13 in progress

--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -1,12 +1,24 @@
-import { render } from "@testing-library/react";
-import RestaurantCard from "../RestaurantCard";
+import { render,screen } from "@testing-library/react";
+import RestaurantCard,{withPromotedLabel} from "../RestaurantCard";
 import resList from "../../utils/mockData";
-
+import "@testing-library/jest-dom";
 describe("Restaurant Card Test Cases",()=>{
     it("should render restaurant cards with props data",()=>{
         const restLists = resList;
         render(
             <RestaurantCard resData={restLists[0]}/>
         );
+        const restName=screen.getByText("Punjabi Angithi (Vegorama Group)");
+        expect(restName).toBeInTheDocument();
+    });
+
+    it("should render with Promoted Label",()=>{
+        const restLists = resList;
+        const RestaurantCardPromoted = withPromotedLabel(RestaurantCard);
+        render(
+            <RestaurantCardPromoted resData={restLists[0]}/>
+        );
+        const promotedRest=screen.getByText("Promoted");
+        expect(promotedRest).toBeInTheDocument();
     });
 });
